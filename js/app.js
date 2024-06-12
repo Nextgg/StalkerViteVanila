4323cf0afb3c5dde4a2e890ad1d629842957a052
nextb <HDNEXT2@GMAIL.COM>
2024-06-12 15:27:23 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll",e=>
+    {
+        document.body.style.cssText +=`--scrollTop: ${scrollY}px`
+    }
+)
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother)
+
+ScrollSmoother.create(
+    {
+        wrapper:'.wrapper',
+        content:'.container'
+    }
+)
